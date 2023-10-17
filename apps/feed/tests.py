330872587cfd6bea2d6ee2cdede8e717e0a142d8
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-10-18 00:24:45 +0100

    tests: posts CRUD

--- apps/feed/tests.py
+++ apps/feed/tests.py
@@ -1,3 +1,254 @@
 from django.test import TestCase
+from django.test.client import AsyncClient
+from unittest import mock
+from apps.feed.models import Post, Reaction, Comment, Reply
+from apps.common.utils import TestUtil
+from apps.common.error import ErrorCode
+import uuid
 
-# Create your tests here.
+
+class TestFeed(TestCase):
+    posts_url = "/api/v2/feed/posts/"
+    reactions_url = "/api/v2/feed/reactions/"
+    comment_url = "/api/v2/feed/comments/"
+    reply_url = "/api/v2/feed/replies/"
+
+    maxDiff = None
+
+    def setUp(self):
+        self.client = AsyncClient()
+        self.content_type = "application/json"
+
+        # user
+        verified_user = TestUtil.verified_user()
+        another_verified_user = TestUtil.another_verified_user()
+        self.verified_user = verified_user
+
+        # post
+        post = Post.objects.create(
+            author=verified_user, text="This is a nice new platform"
+        )
+        self.post = post
+
+        # auth
+        auth_token = TestUtil.auth_token(verified_user)
+        self.bearer = {"Authorization": f"Bearer {auth_token}"}
+        auth_token = TestUtil.auth_token(another_verified_user)
+        self.other_user_bearer = {"Authorization": f"Bearer {auth_token}"}
+
+        # reaction
+        self.reaction = Reaction.objects.create(
+            user=verified_user, rtype="LIKE", post=post
+        )
+
+        # comment
+        comment = Comment.objects.create(
+            author=verified_user, post=post, text="Just a comment"
+        )
+        self.comment = comment
+
+        # reply
+        self.reply = Reply.objects.create(
+            author=verified_user, comment=comment, text="Simple reply"
+        )
+
+    async def test_retrieve_posts(self):
+        post = self.post
+        response = await self.client.get(self.posts_url, content_type=self.content_type)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "Posts fetched",
+                "data": {
+                    "per_page": 50,
+                    "current_page": 1,
+                    "last_page": 1,
+                    "posts": [
+                        {
+                            "author": mock.ANY,
+                            "text": post.text,
+                            "slug": post.slug,
+                            "reactions_count": mock.ANY,
+                            "comments_count": mock.ANY,
+                            "image": None,
+                            "created_at": mock.ANY,
+                            "updated_at": mock.ANY,
+                        }
+                    ],
+                },
+            },
+        )
+
+    async def test_create_post(self):
+        post_dict = {"text": "My new Post"}
+        response = await self.client.post(
+            self.posts_url, post_dict, content_type=self.content_type, **self.bearer
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "Post created",
+                "data": {
+                    "author": mock.ANY,
+                    "text": post_dict["text"],
+                    "slug": mock.ANY,
+                    "created_at": mock.ANY,
+                    "updated_at": mock.ANY,
+                    "file_upload_data": None,
+                },
+            },
+        )
+
+    async def test_retrieve_post(self):
+        post = self.post
+
+        # Test for post with invalid slug
+        response = await self.client.get(
+            f"{self.posts_url}invalid-slug/", content_type=self.content_type
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "code": ErrorCode.NON_EXISTENT,
+                "message": "No post with that slug",
+            },
+        )
+
+        # Test for post with valid slug
+        response = await self.client.get(
+            f"{self.posts_url}{post.slug}/", content_type=self.content_type
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "Post Detail fetched",
+                "data": {
+                    "author": mock.ANY,
+                    "text": post.text,
+                    "slug": post.slug,
+                    "reactions_count": mock.ANY,
+                    "comments_count": mock.ANY,
+                    "image": None,
+                    "created_at": mock.ANY,
+                    "updated_at": mock.ANY,
+                },
+            },
+        )
+
+    async def test_update_post(self):
+        post_dict = {"text": "Post Text Updated"}
+        post = self.post
+        # Check if endpoint fails for invalid post
+        response = await self.client.put(
+            f"{self.posts_url}invalid-slug/",
+            post_dict,
+            content_type=self.content_type,
+            **self.bearer,
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "code": ErrorCode.NON_EXISTENT,
+                "message": "No post with that slug",
+            },
+        )
+
+        # Check if endpoint fails for invalid owner
+        response = await self.client.put(
+            f"{self.posts_url}{post.slug}/",
+            post_dict,
+            content_type=self.content_type,
+            **self.other_user_bearer,
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "code": ErrorCode.INVALID_OWNER,
+                "message": "This Post isn't yours",
+            },
+        )
+
+        # Check if endpoint succeeds if all requirements are met
+        response = await self.client.put(
+            f"{self.posts_url}{post.slug}/",
+            post_dict,
+            content_type=self.content_type,
+            **self.bearer,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "Post updated",
+                "data": {
+                    "author": mock.ANY,
+                    "text": post_dict["text"],
+                    "slug": mock.ANY,
+                    "created_at": mock.ANY,
+                    "updated_at": mock.ANY,
+                    "file_upload_data": None,
+                },
+            },
+        )
+
+    async def test_delete_post(self):
+        post = self.post
+        # Check if endpoint fails for invalid post
+        response = await self.client.delete(
+            f"{self.posts_url}invalid-slug/",
+            content_type=self.content_type,
+            **self.bearer,
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "code": ErrorCode.NON_EXISTENT,
+                "message": "No post with that slug",
+            },
+        )
+
+        # Check if endpoint fails for invalid owner
+        response = await self.client.delete(
+            f"{self.posts_url}{post.slug}/",
+            content_type=self.content_type,
+            **self.other_user_bearer,
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "code": ErrorCode.INVALID_OWNER,
+                "message": "This Post isn't yours",
+            },
+        )
+
+        # Check if endpoint succeeds if all requirements are met
+        response = await self.client.delete(
+            f"{self.posts_url}{post.slug}/",
+            content_type=self.content_type,
+            **self.bearer,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "Post deleted",
+            },
+        )
