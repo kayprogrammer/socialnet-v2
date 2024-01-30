4fa28e72fdc5138fa20a57edfa7131b11523667c
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2024-01-30 14:32:48 +0100

    updated socket

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.chat import consumers
 
 chatsocket_urlpatterns = [
-    path("api/v2/ws/chats/<uuid:id>/", consumers.ChatConsumer.as_asgi())
+    path("api/v2/ws/chats/<str:id>/", consumers.ChatConsumer.as_asgi())
 ]
